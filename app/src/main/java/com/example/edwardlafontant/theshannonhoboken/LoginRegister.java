c7955c057c896dc5834493bd84d188af42033631
Barapps <lafontante1@montclair.edu>
2016-02-25 17:57:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/edwardlafontant/theshannonhoboken/LoginRegister.java
@@ -0,0 +1,15 @@
+package com.example.edwardlafontant.theshannonhoboken;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class LoginRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+
+    }
+}
